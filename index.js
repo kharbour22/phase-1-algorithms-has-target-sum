195ea8fd736d98ffbe2036153be52cb713924d8e
KevinHarbour <kharbour22@gmail.com>
2024-02-14 11:18:41 -0500

    algo

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  const numSet = new Set();
+
+  for (let num of array) {
+      const complement = target - num;
+      if (numSet.has(complement)) {
+          return true;
+      }
+      numSet.add(num);
+  }
+
+  return false;
 }
+  
 
 /* 
   Write the Big O time complexity of your function here
